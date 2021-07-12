4ec784fd8cf838df9a7a83547370fc3aca688c9f
Mass Didier Coly <mass-didier.coly@expleogroup.com>
2021-07-12 14:23:42 +0200

    Ajout message de recette non trouvée

--- js/search/SearchServices/SearchService.js
+++ js/search/SearchServices/SearchService.js
@@ -7,30 +7,33 @@ import MainSearch from "./MainSearch.js";
 class SearchService {
     constructor() {
         this.recipes = RecipesDB;
-        this.searchResultFinal = this.recipes; //50
     }
 
     launch() {
         this.searchParams = new SearchParams();
         this.searchResult = new SearchResult();
-        console.log('vide' + this.searchParams.hasNoSearch());
+        this.searchResultFinal = this.recipes; //50
         console.log('input' +this.searchParams.isValidForPrimarySearch());
         console.log('tag' +this.searchParams.isValidForSecondarySearch());
-        
-        if (this.searchParams.hasNoSearch()) {
-            this.searchResultFinal = RecipesDB; //50
-        }
 
         // si texte présent dans le input principal lance une recherche
         if (this.searchParams.isValidForPrimarySearch()) {
             this.searchResultPrincipale = MainSearch.research(this.searchParams); //30
-            this.searchResultFinal = this.searchResultPrincipale;
+            this.searchResultFinal = this.searchResultPrincipale;   
         }
+        //Affiche message recette non trouvée
+        if (this.searchResultFinal.size === 0) {
+            document.getElementById('notFoundRecipes').classList.remove('notFound');
+        } else{
+            document.getElementById('notFoundRecipes').classList.add('notFound');
+        }
+        
         // si uniquement tag selectionné
         if (this.searchParams.isValidForSecondarySearch()) {            
             this.searchResultSecondary = MainSearch.tagsresearch(this.recipes, this.searchParams);
             this.searchResultFinal = this.searchResultSecondary;
         }
+
          this.searchResult.buildRecipes(this.searchResultFinal);
         console.dir(this);
         return this.searchResult;
